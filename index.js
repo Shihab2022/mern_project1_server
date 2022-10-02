9e06e233c24e976193388e6759a9c880f9a04c8d
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-10-02 22:12:40 +0600

    add more code

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// require('dotenv').config();
-
-
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ywpqpha.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const dataCollection = client.db("mern_project1").collection("data");
+ 
+app.get('/allData',async(rep,res)=>{
+
+    const tools = await dataCollection.find({}).toArray();
+    console.log(tools)
+    res.send('tools',tools);
+})
+
+
+    } 
+    
+    finally {
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
